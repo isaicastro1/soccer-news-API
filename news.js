6101cf215fe54ba58d83fc1dbe9470894f5077d1
Isai Castro <isaicstr@gmail.com>
2022-12-19 18:01:18 -0700

    Added one more item tonews

--- news.js
+++ news.js
@@ -4,7 +4,7 @@ export const news = [
     description:
       "Messi didn't need that golden cup to secure his seat at the G.O.A.T. table. He wanted it to give back to his teammates and to his nation, after 22 years abroad (and counting).",
     image:
-      "https://a2.espncdn.com/combiner/i?img=%2Fphoto%2F2022%2F1218%2Fr1108379_1296x729_16%2D9.jpg&amp;w=375, https://a2.espncdn.com/combiner/i?img=%2Fphoto%2F2022%2F1218%2Fr1108379_1296x729_16%2D9.jpg&amp;w=750&amp;cquality=40&amp;format=jpg 2x",
+      "https://a2.espncdn.com/combiner/i?img=%2Fphoto%2F2022%2F1218%2Fr1108379_1296x729_16%2D9.jpg&amp;w=375",
     source: "ESPN",
     details:
       "https://www.espn.com/soccer/argentina-arg/story/4834110/what-world-cup-2022-win-does-for-lionel-messi-legend",
@@ -50,5 +50,15 @@ export const news = [
     source: "ESPN",
     details:
       "https://www.espn.com/soccer/soccer-transfers/story/4833548/christian-pulisic-concentrating-on-chelsea-but-doesnt-rule-out-transfer",
+  },
+  {
+    title: "Bayern Munich's Manuel Neuer breaks leg skiing, out for season",
+    description:
+      "Manuel Neuer suffered a leg fracture while on a skiing tour, the Germany and Bayern Munich goalkeeper said, ruling him out for the rest of the season.",
+    image:
+      "https://scontent.cdninstagram.com/v/t51.2885-15/319078048_880021146337948_1808153253650726111_n.jpg?stp=dst-jpg_e35_p1080x1080&_nc_ht=scontent.cdninstagram.com&_nc_cat=104&_nc_ohc=zlJ4sHAw0FYAX_Z99y1&edm=AJBgZrYBAAAA&ccb=7-5&oh=00_AfBzlj3-n3gs9ULYO_83LkKiQ2WzMIaXUcbe1S8S5e5ffA&oe=63A5D268&_nc_sid=78c662",
+    source: "ESPN",
+    details:
+      "https://www.espn.com/soccer/bayern-munich-gerbayern_munich/story/4829981/bayern-munichs-manuel-neuer-breaks-leg-skiingout-for-season",
   },
 ];
